e155b09c1b4a1dce6b91c4528eccaf8aaed896bd
BigBrother1984 <carlosavignano@aospa.co>
2015-01-04 15:42:04 +0100

    Add sensors group
    Signed-off-by: @BigBrother1984 <carlosavignano@aospa.co>
    Change-Id: I0ab313cd7cef396927904c087e59e98ee2874164

--- include/private/android_filesystem_config.h
+++ include/private/android_filesystem_config.h
@@ -95,6 +95,9 @@
 #define AID_NET_BT_STACK  3008  /* bluetooth: access config files */
 
 #define AID_EVERYBODY     9997  /* shared between all apps in the same profile */
+
+#define AID_SENSORS       3012 /* access to /dev/socket/sensor_ctl_socket & QCCI/QCSI */
+
 #define AID_MISC          9998  /* access to misc storage */
 #define AID_NOBODY        9999
 
@@ -171,6 +174,7 @@ static const struct android_id_info android_ids[] = {
     { "net_bt_stack",  AID_NET_BT_STACK, },
 
     { "everybody",     AID_EVERYBODY, },
+    { "sensors",       AID_SENSORS, },
     { "misc",          AID_MISC, },
     { "nobody",        AID_NOBODY, },
 };

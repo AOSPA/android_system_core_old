b1d91e50604c1ce163f1fe734b9ed8e1a980cac5
Lennart Wieboldt <lennart.1997@gmx.de>
2017-07-15 22:58:24 +0200

    Revert "Particular revert "Add more cgroups to the schedtune cgroup hierarchy""
    This reverts commit 3fcb1c521c51b97ddc1761fcd365704cb19866b0.
    
    This is no longer needed Performance issues were caused because
    schedtune.boost for top-app was not created back then due to
    system-background overwriting it.
    But this commit fixed the overwriting and allows schedtune/top-app
    and schedtune/system-background to co-exist:
    https://github.com/AOSPA/android_kernel_google_marlin/commit/705b624434fffe9d16a88071e96aae7366d13d50
    Thanks to @joshuous
    
    Change-Id: Ic6fbdb75dff1678704e88567c262194df4d4430c

--- libcutils/sched_policy.cpp
+++ libcutils/sched_policy.cpp
@@ -59,6 +59,7 @@ static int system_bg_cpuset_fd = -1;
 static int bg_cpuset_fd = -1;
 static int fg_cpuset_fd = -1;
 static int ta_cpuset_fd = -1; // special cpuset for top app
+static int system_bg_schedboost_fd = -1;
 
 // File descriptors open to /dev/stune/../tasks, setup by initialize, or -1 on error
 static int bg_schedboost_fd = -1;
@@ -310,6 +311,7 @@ int set_cpuset_policy(int tid, SchedPolicy policy)
         break;
     case SP_SYSTEM:
         fd = system_bg_cpuset_fd;
+        boost_fd = system_bg_schedboost_fd;
         break;
     default:
         boost_fd = fd = -1;
